39135a4e5271127ca858ed1f6bd8ef8e76985ff2
krishvaghasiya705 <kvaghasiya705@gmail.com>
2024-04-18 17:53:03 +0530

    some changes of page

--- src/errorpage/index.js
+++ src/errorpage/index.js
@@ -9,62 +9,84 @@ import red from "../assets/image/Red1.png";
 import ring from "../assets/image/Ellipse673.png";
 import logo from "../assets/image/image1.png";
 import lock from "../assets/image/ic_round-lock.png";
+import crose from "../assets/image/crose.png";
+import sorry from "../assets/image/Group 239205.png";
 import { Link } from 'react-router-dom';
 
 export default function Errorpage() {
     return (
         <div>
-            <div className="ellepse-background-color-main-one">
-                <div className='ellepse-black-height-box'>
-                    <div className="container-header">
-                        <header>
-                            <div className="logo">
-                                <img src={logo} alt="logo" />
-                            </div>
-                            <div className="headert-all-links-main">
-                                <a href="Authenticate">Authenticate</a>
-                                <Link to={"/product"}>Products</Link>
-                                <a href="Lab Results">Lab Results</a>
-                                <a href="Contact Us">Contact Us</a>
-                            </div>
-                            <div className="lock-div-main">
-                                <img src={lock} alt="lock" />
-                            </div>
-                        </header>
-                    </div>
-                    <div className="container-header-one">
-                        <div className="discover-line-main">
-                            <h5>Discover, Authenticate, and Shop our <span>premium cannabis products</span></h5>
+            <div className="alert-back-color">
+                <div className="ellepse-background-color-main-one">
+                    <div className='ellepse-black-height-box'>
+                        <div className="container-header">
+                            <header>
+                                <div className="logo">
+                                    <img src={logo} alt="logo" />
+                                </div>
+                                <div className="headert-all-links-main">
+                                    <a href="Authenticate">Authenticate</a>
+                                    <Link to={"/product"}>Products</Link>
+                                    <a href="Lab Results">Lab Results</a>
+                                    <a href="Contact Us">Contact Us</a>
+                                </div>
+                                <div className="lock-div-main">
+                                    <img src={lock} alt="lock" />
+                                </div>
+                            </header>
                         </div>
-                    </div>
+                        <div className="container-header-one">
+                            <div className="discover-line-main">
+                                <h5>Discover, Authenticate, and Shop our <span>premium cannabis products</span></h5>
+                            </div>
+                        </div>
 
-                </div>
-                <div className='ellepse-black-height-box2'>
-                    <div className="tko-extract-box-main">
-                        <div className="tko-conatiner-image">
-                            <img src={red} alt="red" />
+                    </div>
+                    <div className='ellepse-black-height-box2'>
+                        <div className="tko-extract-box-main">
+                            <div className="tko-conatiner-image">
+                                <img src={red} alt="red" />
+                            </div>
+                        </div>
+                        <div className="ring-main">
+                            <img src={ring} alt="ring" />
                         </div>
                     </div>
-                    <div className="ring-main">
-                        <img src={ring} alt="ring" />
+                    <div className="top-ellepse-shadow">
+                        <img src={Ellipse} alt="ellepse" />
                     </div>
-                </div>
-                <div className="top-ellepse-shadow">
-                    <img src={Ellipse} alt="ellepse" />
-                </div>
-                <div className="moon-div-main">
-                    <div className="moon1">
-                        <img src={moon} alt="moon" />
-                    </div>
-                    <div className="moon2">
-                        <img src={moon2} alt="moon" />
-                    </div>
-                    <div className="cloud-main">
-                        <div className="cloud1">
-                            <img src={cloud1} alt="cloud1" />
+                    <div className="moon-div-main">
+                        <div className="moon1">
+                            <img src={moon} alt="moon" />
                         </div>
-                        <div className="cloud2">
-                            <img src={cloud2} alt="cloud2" />
+                        <div className="moon2">
+                            <img src={moon2} alt="moon" />
+                        </div>
+                        <div className="cloud-main">
+                            <div className="cloud1">
+                                <img src={cloud1} alt="cloud1" />
+                            </div>
+                            <div className="cloud2">
+                                <img src={cloud2} alt="cloud2" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="error-bakc-blure">
+                    <div className="error-back-box">
+                        <a href="crose">
+                            <div className="crose-div">
+                                <img src={crose} alt="crose" />
+                            </div>
+                        </a>
+                        <div className="sorrry-image">
+                            <img src={sorry} alt="sorry" />
+                        </div>
+                        <div className="sorry-title">
+                            <h1>Sorry 404 Error</h1>
+                        </div>
+                        <div className="sorry-pera">
+                            <p>You are not old enough to view  the site...</p>
                         </div>
                     </div>
                 </div>
