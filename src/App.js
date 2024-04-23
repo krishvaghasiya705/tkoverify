163cdecb8ad6e75445e4624777116937e9977a7c
krishvaghasiya705 <kvaghasiya705@gmail.com>
2024-04-23 17:46:45 +0530

    complate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
-import { useEffect } from 'react';
 import './App.css';
 import router from './routes';
-import Aos from 'aos';
 
 function App() {
-  useEffect(() => {
-    Aos.init({ duration: 1000 });
-  }, [])
   return (
     <RouterProvider router={router} />
   );
